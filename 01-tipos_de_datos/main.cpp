6967a27bf5236b9e3fa2f6b8a9553a8d9c43da4c
Alexis Aguirre <Alexis Aguirre alex08lta@gmail.com>
2020-07-14 15:56:41 -0300

    agregados tipos de datos

--- 01-tipos_de_datos/main.cpp
+++ 01-tipos_de_datos/main.cpp
@@ -1,22 +1,37 @@
 #include <iostream>
+using namespace std;
 
 int main(int argc, char *argv[]) {
-    setlocale(LC_CTYPE, "Spanish");
 
-    std::cout << "Bienvenido a la clase de tipos de datos.\n";
-    std::cout << "\n";
+    cout << "Bienvenido a la clase de tipos de datos.\n";
+    cout << "\n";
 
-    int numeroEntero = 3;
-    std::cout << "Un numero entero se define con la palabra clave 'int'.\nint numeroEntero = 3;\n";
-    std::cout << numeroEntero << "\n\n";
+    // int numeroEntero = 3;
+    // cout << "Un numero entero se define con la palabra clave 'int'.\nint numeroEntero = 3;\n";
+    // cout << numeroEntero << "\n\n";
 
-    float numeroFlotante = 3.3;
-    std::cout << "Un numero de punto flotante se define con la palabra clave 'float' o 'double'.\nfloat numeroFlotante = 3.3;" << "\n";
-    std::cout << numeroFlotante << "\n\n";
+    // short int enteroCorto = 32767; // entre -32768 y 32767
+    // unsigned short int enteroCortoSinSigno = 65535; // entre 0 y 65535
 
-    char caracter1 = 'c';
-    std::cout << "Un caracter se define con la palabra clave 'char'.\nchar caracter1 = 'c';\n";
-    std::cout << caracter1 << "\n\n";
+    // float numeroFlotante = 3.3;
+    // cout << "Un numero de punto flotante se define con la palabra clave 'float' o 'double'.\nfloat numeroFlotante = 3.3;";
+    // cout << numeroFlotante << "\n\n";
 
-    system("PAUSE");
+    // char caracter1 = 'c';
+    // cout << "Un caracter se define con la palabra clave 'char'.\nchar caracter1 = 'c';\n";
+    // cout << caracter1 << "\n\n";
+
+    // bool bandera1 = false;
+    // bool bandera2 = true;
+    // cout << "Un booleano es una variable binaria, es decir, solo puede tomar dos valores.\nbool bandera = false;\n" ;
+    // cout << bandera1 << " " << bandera2 << endl;
+
+    // int numeros[5] = {1,2,3,4,5};
+
+    // for(int i = 0 ; i < sizeof(numeros) / sizeof(*numeros) ; i++) {
+    //     cout << numeros[i] << endl;
+    // }
+
+    // system("PAUSE");
+
 }
