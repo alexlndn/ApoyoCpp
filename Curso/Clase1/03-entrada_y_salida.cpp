925c75fafe4e520bc67f4d8ad260dfa138335269
Alexis Aguirre <Alexis Aguirre alex08lta@gmail.com>
2020-07-17 23:09:22 -0300

    estructuración del curso

---
+++ Curso/Clase1/03-entrada_y_salida.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // SALIDA DE TEXTO
+    cout << "Este texto se imprime en pantalla." << endl;
+
+    // ENTRADA DE TEXTO
+    int entrada;
+    cin >> entrada;
+    // lo que el usuario ingrese quedará guardado en la variable entrada.
+
+    // imprimir una variable en pantalla
+
+    cout << "Valor de la variable 'entrada'" << entrada << endl;
+
+    // endl indica un salto de linea
+
+    cout << "\t" << endl; // tabulador
+    cout << "\n" << endl; // nueva linea (al igual que el endl)
+
+
+    return 0;
+}
