d566359b076de7ad48409568bd714ba43611cb18
Alexis Aguirre <Alexis Aguirre alex08lta@gmail.com>
2020-07-20 15:56:14 -0300

    ejercicio agregado

---
+++ Curso/Clase2/Ejercicios/01.cpp
@@ -0,0 +1,23 @@
+/*
+    Escriba un programa que solicite al usuario un numero entero positivo (o cero)
+
+    si el numero ingresado por el usuario es menor a 10
+    el programa debera imprimir los numeros desde el 0 hasta el numero ingresado,
+    en caso contrario el programa debera imprimir los numeros desde el cero hasta el numero ingresado, sumando de a 10, y si
+    el ingresado no es un multiplo de 10, este debera mostrarse tambien.
+
+    Ejemplos:
+    el usuario ingresa el 72, la salida del programa será:
+    0 10 20 30 40 50 60 70 72
+
+    el usuario ingresa el 8:
+    0 1 2 3 4 5 6 7 8
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
