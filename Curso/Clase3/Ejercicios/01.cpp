9266bb109ae9ae035327f0c57b77a2bc8b1fa11b
Alexis Aguirre <Alexis Aguirre alex08lta@gmail.com>
2020-07-23 09:56:10 -0300

    se agrego un ejercicio de la clase 3

---
+++ Curso/Clase3/Ejercicios/01.cpp
@@ -0,0 +1,45 @@
+/*
+    Escriba un programa que utilice las funciones sumar y restar,
+    el programa solicitara al usuario el ingreso de un valor (operacion)
+
+    ## El usuario deberá declarar estas funciones utilizando los prototipos
+    correspondientes para realizar las operaciones con dos numeros. ##
+
+    El programa actuara dependiendo del valor ingresado
+
+    si el valor ingresado es 1: el programa solicitara al usuario ingresar
+    dos valores e imprimira la suma de estos valores
+
+    si el valor ingresado es 2: el programa solicitara al usuario ingresar
+    dos valores e imprimira la resta de estos valores
+
+    si el valor ingresado es cualquier otro valor distinto de 1 o 2
+    el programa debe terminar.
+
+    Ejemplo de ejecucion 1:
+
+    Ingrese 1 o 2: 1
+    Ingrese el primer numero: 31
+    Ingrese el segundo numero: 12
+    La suma es: 43
+
+    Ejemplo de ejecucion 2:
+
+    Ingrese 1 o 2: 2
+    Ingrese el primer numero: 31
+    Ingrese el segundo numero: 12
+    La diferencia es: 19
+
+    Ejemplo de ejecucion 3:
+    Ingrese 1 o 2: 3
+*/
+
+#include <iostream>
+using namespace std;
+// prototipos de funciones
+
+int main() {
+
+}
+
+// declaracion de funciones
