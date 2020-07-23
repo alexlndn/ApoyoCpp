61bfa13004468935ac727524d6a4d8e058bc78f1
Alexis Aguirre <alex08lta@gmail.com>
2020-07-23 11:40:52 -0300

    agregados dos ejercicios iniciales

---
+++ Curso/Clase2/Ejercicios/01.cpp
@@ -0,0 +1,23 @@
+/*
+    Solicite al usuario el ingreso de un numero entero
+    Si el numero ingresado es par el programa deberá calcular
+    la potencia que como base utilice el numero ingresado por el usuario
+    y como exponente el numero 2 e imprimir:
+    EL NUMERO x ES PAR.
+    SU CUADRADO ES (x^2)
+
+    Si el numero ingresado es impar el programa deberá calcular
+    el modulo del numero con respecto 3 (x % 3) y calcular la raiz cuadrada
+    del numero resultante del modulo e imprimir:
+    EL NUMERO x ES IMPAR.
+    LA RAIZ DEL MODULO 3 ES (valor)
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
---
+++ Curso/Clase2/Ejercicios/02.cpp
@@ -0,0 +1,46 @@
+/*
+    Escriba un programa que solicite al usuario ingresar 3 valores,
+    el programa calculará la suma de estos numeros y le pedira al usuario
+    que ingrese un cuarto valor.
+
+    El programa debe calcular el modulo de la suma de los primeros tres numero con el cuarto numero.
+
+    si el VR (valor resultante) es verdadero (recordar equivalencias de valores)
+    el programa procederá a decidir de acuerdo a ese valor:
+        - si VR es 1:
+            imprimir el cuadrado de VR
+        - si VR es 2:
+            imprimir el cubo de VR
+        - si VR es 3:
+            imprimir la raiz cuadrada de VR
+        - en cualquier otro caso:
+            imprimir VR
+    
+    si el VR es falso:
+        el programa solicitará al usuario ingresar otro valor e imprimira la raiz cuarta de ese valor.
+
+    Ejemplo de ejecucion 1:
+
+    Ingrese 3 valores: 5 8 12
+    Ingrese un valor: 2
+    1
+
+    Ejemplo de ejecucion 2:
+    Ingrese 3 valores: 3 7 15
+    Ingrese un valor: 5
+    Ingrese un valor: 16
+    2
+
+    Ejemplo de ejecucion 3:
+    Ingrese 3 valores: 3 9 15
+    Ingrese un valor: 6
+    1.7320
+*/
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+
+    return 0;
+}
