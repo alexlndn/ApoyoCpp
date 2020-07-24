9b03322e5944d4334f0c019bf4b45307bd750bd1
Alexis Aguirre <Alexis Aguirre alex08lta@gmail.com>
2020-07-24 17:42:35 -0300

    se agregó un ejercicio de la clase 3

---
+++ Curso/Clase3/Ejercicios/02.cpp
@@ -0,0 +1,20 @@
+/*
+    Escriba la funcion cuenta_regresiva que recibe un entero,
+    esta funcion deberá ser una función recursiva que realice la cuenta
+    regresiva del numero que recibe como parámetro.
+
+    El programa (main) deberá solicitar al usuario que ingrese un numero entero positivo,
+    con ese numero deberá realizar la cuenta regresiva.
+*/
+
+#include <iostream>
+using namespace std;
+
+void cuenta_regresiva(int);
+
+int main() {
+    // complete el programa principal
+    return 0;
+}
+
+// defina el cuerpo de la funcion solicitada.
