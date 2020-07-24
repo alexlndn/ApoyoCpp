5346228ca974a9020452c8656856888445840467
Alexis Aguirre <Alexis Aguirre alex08lta@gmail.com>
2020-07-24 17:47:36 -0300

    .

--- test.cpp
+++ test.cpp
@@ -3,9 +3,6 @@
 using namespace std;
 
 int main() {
-    int d = 8 / 2; // division
-    cout << "division: " << d << endl;
-    int e = 8 % 2; // modulo
-    cout << "modulo: " << e << endl;
+
     return 0;
 }
